f6dbdfbb42aa1f951251717119d0cef61a3d5dba
MiladSadeghi <miladsadeghi2323@gmail.com>
2023-06-13 18:57:27 +0330

    feat: adding fav icons, adding license, adding README

--- server/src/api/controller/insights.controller.js
+++ server/src/api/controller/insights.controller.js
@@ -3,22 +3,29 @@ import { PropertyViewModel } from "../models/views.model.js";
 
 export const propertyInsight = async (req, res) => {
   const { propertyId, agentId } = req.params;
+
+	const today = new Date();
+  today.setHours(0, 0, 0, 0);
+
   const viewedToday =
-    req.session.views && req.session.views[propertyId] === new Date().getDate();
+    req.session.views && req.session.views.includes(propertyId);
 
   if (!viewedToday) {
     await PropertyViewModel.updateOne(
       {
         property: new mongoose.Types.ObjectId(propertyId),
         agent: new mongoose.Types.ObjectId(agentId),
+        createdAt: { $gte: today },
       },
-      { $inc: { count: 1 } },
+      {
+        $inc: { count: 1 },
+      },
       { upsert: true }
     );
     if (!req.session.views) {
-      req.session.views = {};
+      req.session.views = [];
     }
-    req.session.views[propertyId] = new Date().getDate();
+    req.session.views.push(propertyId);
   }
 
   return res.sendStatus(200);
