e86b170c276d194be7332f427aecf48ba123d636
MiladSadeghi <miladsadeghi2323@gmail.com>
2023-06-17 17:04:08 +0330

    refactor: refactoring PWA
    remove vite-plugin-pwa and do it manually

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+// import App from "./App.tsx";
 import {BrowserRouter} from "react-router-dom";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import axios from "axios";
-import {ToastContainer} from "react-toastify";
-import { registerSW } from "virtual:pwa-register";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./styles/index.css";
 import "react-loading-skeleton/dist/skeleton.css";
@@ -13,30 +12,23 @@ import "swiper/css";
 import "swiper/css/navigation";
 
 axios.defaults.baseURL = `${import.meta.env.VITE_BASE_URL}`;
+const LazyApp = React.lazy(() => import("./App.tsx"));
 
-const updateSW = registerSW({
-  onNeedRefresh() {
-    if (confirm("New content available. Reload?")) {
-      updateSW(true);
-    }
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
   },
 });
 
-const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            staleTime: 1000 * 60 * 5,
-        },
-    },
-});
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <BrowserRouter>
-                <App/>
-                <ToastContainer/>
-            </BrowserRouter>
-        </QueryClientProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <LazyApp />
+        <ToastContainer />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </React.StrictMode>
 );
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,3 @@
-import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -15,47 +14,6 @@ export default defineConfig({
     react({
       babel: {
         plugins: ["babel-plugin-macros", "babel-plugin-styled-components"],
-      },
-    }),
-    VitePWA({
-      workbox: {
-        globPatterns: ["**/*"],
-      },
-      includeAssets: ["**/*"],
-      manifest: {
-        name: "Homelun",
-        short_name: "Homelun",
-        icons: [
-          {
-            src: "/android-chrome-144x144.png",
-            sizes: "144x144",
-            type: "image/png",
-          },
-          {
-            src: "/apple-touch-icon.png",
-            sizes: "144x144",
-            type: "image/png",
-          },
-          {
-            src: "/favicon-16x16.png",
-            sizes: "16x16",
-            type: "image/png",
-          },
-          {
-            src: "/favicon-32x32.png",
-            sizes: "32x32",
-            type: "image/png",
-          },
-          {
-            src: "/favicon.ico",
-            sizes: "32x32",
-            type: "image/ico",
-          },
-        ],
-        theme_color: "#ffffff",
-        background_color: "#ffffff",
-        display: "standalone",
-        start_url: ".",
       },
     }),
   ],
